58447c5a9db117d78b38f93c7a053e670dab9258
Fabio Cruz <fabinhocosta1202@gmail.com>
2024-05-13 19:53:25 -0300

    :sparkles: Add index page route

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.base')
+
+@section('title', 'Nexcent')
+
+@section('content')
+    <div class="container overflow-x-hidden mx-auto flex items-start justify-start flex-col gap-3">
+        Design
+    </div>
+@endsection
