68002f66d98de9c5fbe2f89932af91bfcb969ce4
Fabio Cruz <fabinhocosta1202@gmail.com>
2024-05-13 19:52:40 -0300

    :sparkles: Create header layout

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,15 @@
+<div class="overflow-x-hidden mx-auto py-10 lg:px-32 px-5 flex items-start justify-between flex-row">
+    <img class="-ml-1" src="{{ asset('images/Logo.png') }}" alt="logo" />
+    <div class="hidden md:flex md:flex-row md:gap-12">
+        <a href="#" class="text-body-medium-2">Home</a>
+        <a href="#" class="text-body-regular-2">Service</a>
+        <a href="#" class="text-body-regular-2">Feature</a>
+        <a href="#" class="text-body-regular-2">Product</a>
+        <a href="#" class="text-body-regular-2">Testimonial</a>
+        <a href="#" class="text-body-regular-2">FAQ</a>
+    </div>
+    <div class="flex flex-row gap-3.5">
+        <x-button type="button" style-type="tertiary" size="medium">Login</x-button>
+        <x-button type="button" style-type="primary" size="medium">Sign up</x-button>
+    </div>
+</div>
